197a3d11515d25dda24cacd00fb5a692710beb06
Derek Sharkey <sharks@mako.localdomain>
2020-04-11 10:48:37 -0400

    login endpoint

--- diagramViewer/urls.py
+++ diagramViewer/urls.py
@@ -24,6 +24,7 @@ router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('rest-auth/', include('rest_auth.urls')),
 #   path('', include(router.urls)),
 #   path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
     path('', include('diagram.urls'))
